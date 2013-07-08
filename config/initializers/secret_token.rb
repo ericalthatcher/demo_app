e8dc76941c679078c01905a904f2a95700530ef1
ericalthatcher <ericalthatcher@gmail.com>
2013-07-07 20:26:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Erica::Application.config.secret_key_base = '8430624a77d1b41335641ad6b39eb6aa0c379c6c2bbe48fcb8fcbd3b135a295787092295a7462579ebe91263ddad4a7197e7b335305a4079bddfd326d4d76925'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Erica::Application.config.session_store :cookie_store, key: '_erica_session'
